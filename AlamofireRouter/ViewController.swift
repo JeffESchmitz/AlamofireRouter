feee82ce509fbe753d3a83c4fe11707a05e1c964
Jeff Schmitz <jeffeschmitz@gmail.com>
2017-02-12 07:40:28 -0600

    initial workings of project

--- AlamofireRouter/ViewController.swift
+++ AlamofireRouter/ViewController.swift
@@ -7,12 +7,84 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+  func getFirstPost() {
+    // Get first post
+    let postEndpoint: String = "http://jsonplaceholder.typicode.com/posts/1"
+    
+    Alamofire.request(postEndpoint, method: .get)
+      .responseJSON { (response) in
+        guard response.result.error == nil else {
+          // got an error in getting the data, need to handle it
+          print("error calling GET on /posts/1")
+          print(response.result.error!)
+          return
+        }
+        
+        if let value: Any = response.result.value {
+          // handle the results as JSON, without a bunch of nested if loops
+          let post = JSON(value)
+          // now we have the results, let's just print them though a tableview would definitely be better UI:
+          print("The post is: " + post.description)
+          if let title = post["title"].string {
+            // to access a field:
+            print("The title is: " + title)
+          } else {
+            print("error parsing /posts/1")
+          }
+        }
+    }
+  }
+  
+  func createPost() {
+    let postsEndpoint: String = "http://jsonplaceholder.typicode.com/posts"
+    let newPost = ["title": "Frist Psot", "body": "I iz fisrt", "userId": 1] as [String : Any]
+    
+    Alamofire.request(postsEndpoint, method: .post, parameters: newPost)
+      .responseJSON { (response) in
+        guard response.result.error == nil else {
+          // got an error in getting the data, need to handle it
+          print("error calling GET on /posts/1")
+          print(response.result.error!)
+          return
+        }
+
+        if let value: Any = response.result.value {
+          // handle the results as JSON, without a bunch of nested if loops
+          let post = JSON(value)
+          print("The post is: " + post.description)
+        }
+    }
+  }
+  
+  func deleteFirstPost() {
+    let firstPostEndpoint: String = "http://jsonplaceholder.typicode.com/posts/1"
+    Alamofire.request(firstPostEndpoint, method: .delete)
+      .responseJSON { response in
+        if response.result.isSuccess {
+          print("Delete succeeded on /posts/1")
+        }
+        if let error = response.result.error {
+          // got an error while deleting, need to handle it
+          print("error calling DELETE on /posts/1")
+          print(error)
+        }
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    getFirstPost()
+    
+    createPost()
+
+    deleteFirstPost()
   }
 
   override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AlamofireRouter' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AlamofireRouter
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+
+end
